6df209292c86ada979f9d02785eb3909ccfc3081
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2021-02-01 19:51:30 +0100

    Added useffect to use inout stirng to fetch matching data

--- src/contexts/SearchContext.js
+++ src/contexts/SearchContext.js
@@ -1,13 +1,38 @@
-import { createContext, useState } from 'react';
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
 
 const SearchContext = createContext();
 
 const SearchProvider = ({ children }) => {
 	const [ searchInput, setSearchInput ] = useState('');
+	const [ characters, setCharacters ] = useState(null);
 
 	const getSearchInput = (nameOfCharacter) => {
 		setSearchInput(nameOfCharacter);
 	};
+
+	useEffect(
+		() => {
+			const source = axios.CancelToken.source();
+
+			const getData = async () => {
+				try {
+					const response = await axios.get(`https://rickandmortyapi.com/api/character/?name=${searchInput}`);
+					setCharacters(response.data.results);
+				} catch (error) {
+					console.error('error', error);
+					setCharacters('error');
+				}
+			};
+
+			getData();
+
+			return () => {
+				source.cancel();
+			};
+		},
+		[ searchInput ]
+	);
 
 	return <SearchContext.Provider value={{ getSearchInput, searchInput }}>{children}</SearchContext.Provider>;
 };
